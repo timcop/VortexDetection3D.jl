d105281a8ca77720377610e57474eeddcb0c4cbf
timcop <timothy.copland98@gmail.com>
2021-01-26 00:08:07 +1300

    initial

---
+++ vortex_example.jl
@@ -0,0 +1,27 @@
+gr(xlabel="x",ylabel="y",legend=false)
+
+# make a simple 2D test field
+Nx = 400; Ny = Nx
+Lx = 200; Ly = Lx
+x = LinRange(-Lx / 2, Ly / 2, Nx); y = x
+psi0 = one.(x*y') |> complex
+
+# doubly periodic boundary conditions
+psi = Torus(psi0,x,y)
+
+# make a point vortex
+pv = PointVortex(30.0,70.3,-1)
+
+# make a scalar GPE vortex with exact core
+spv = ScalarVortex(pv)
+vortex!(psi,spv)
+
+# make some more random vortices
+vort = rand_vortex(10,psi)
+vortex!(psi,vort)
+
+vortex_array(pv)
+
+vfound = findvortices(psi)
+
+@btime vort = findvortices(psi)
