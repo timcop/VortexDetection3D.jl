c053c7c1d488da4c101733ca82f8c0e23b785962
timcop <timothy.copland98@gmail.com>
2021-02-11 12:50:36 +1300

    moved functions to functions.jl, deleted files

--- random.jl
+++
@@ -1,8 +0,0 @@
-x = LinRange(-100, 100, 100);
-y = LinRange(-100, 100, 100);
-X = [x, y];
-
-arr = rand(100, 100) + im*rand(100, 100)
-grad = gradient_2D(arr, X);
-
-wps_v_2D(grad, [50, 50])
--- wps_testing.jl
+++ wps_testing.jl
@@ -1,32 +1,14 @@
-# Testing wps at different z levels
-tslice = 3;
-zslice = 16;
-which_vort = 1;
-psi = xspace(sol(tslice), sim);
-grad = gradient_3D_cent(psi, X);
-vorts_zslice = vortex_array(findvortices(Torus(psi[:, :, zslice], x, y)))
-v1 = vorts_zslice[which_vort, :]
-wps_v = wps_int(grad, X, [v1[2], v1[1], z[zslice]])
-
-wps_v = wps_v ./ abs(wps_v[3]) #normalise so z component is 1
-vorts_zslice_under = vortex_array(findvortices(Torus(psi[:, :, zslice-1], x, y)))
-vorts_zslice_above = vortex_array(findvortices(Torus(psi[:, :, zslice+1], x, y)))
-
-v1[1:2]
-v_above = vorts_zslice_above[which_vort, 1:2]
-v_under = vorts_zslice_under[which_vort, 1:2]
-pseudo_vort = v1[1:2] + wps_v[1:2]
-
 ################################
 #wps array/heatmap
+psi = xspace(sol(tslice), sim);
+grad = gradient_3D_cent(psi, X);
+grad_int = grad_itp(grad, X)
 
 Nx = 10; Ny = 10;
 A = LinRange(x[1], x[end], Nx); B = A;
 zslice = 16;
 tslice = 10;
-psi = xspace(sol(tslice), sim);
-grad = gradient_3D_cent(psi, X);
-grad_int = grad_itp(grad, X)
+
 
 wps_array = zeros(Nx, Ny)
 
@@ -47,7 +29,32 @@ X = [x, y, z]
 myarr = rand(128, 128, 32) + im*rand(128, 128, 32);
 grad = gradient_3D_cent(myarr, X)
 grad_int = grad_itp(grad, X)
-grad_int_r = grad_int[1
+grad_int_r = grad_int[1];
+####################
+
+
+## Setup (params)
+tslice = 20;
+zslice = 16;
+dz = z[2]-z[1]
+psi = xspace(sol(tslice), sim);
+grad = gradient_3D_cent(psi, X);
+grad_int = grad_itp(grad, X)
+
+# Vortices at current slice and slice above
+vorts_zslice = vortex_array(findvortices(Torus(psi[:, :, zslice], x, y)))
+vorts_zslice_up = vortex_array(findvortices(Torus(psi[:, :, zslice+1], x, y)))
+
+# Pick a vort and check difference in coordinates, has to be positive charge
+num_vort = 2
+vort = vorts_zslice[num_vort, :]
+vort_up = vorts_zslice_up[num_vort, :]; 
+vort_diff = vort_up[1:2] - vort[1:2];
+
+# Find pseudo vorticity and normalise it to wps_z = dz
+wps = wps_int(grad_int, [vort[1], vort[2], z[zslice]])
+wps_norm = wps .* (dz/abs(wps[3]))
+vort_diff
 
 
 
