f8f83c527f895c640235b594bc2636e868bd5a74
Tim Copland <timothy.copland98@gmail.com>
2022-09-12 16:51:54 +1200

    cleaning up

--- analysis/detection_example.jl
+++ analysis/detection_example.jl
@@ -1,10 +1,11 @@
-## Imports
 using VortexDistributions
 using FourierGPE
 using JLD2
 
 
+
 ## Sim params
+
 L=(16.,16.,16.);
 N=(64,64,64);
 sim = Sim(L,N);
@@ -18,19 +19,12 @@ t = LinRange(0.,tf,Nt);
 
 x,y,z = X;
 
-## Load data 
-# data = joinpath(@__DIR__, "../data/64quench4heal.bson")
 data = joinpath(@__DIR__, "../data/box_vorts.jld2")
 
-# BSON.@load data sol
 @load data psi_tubes1 X
 
-## Pick a psi and plot it
-# psi = xspace(sol(25.), sim);
 psi = psi_tubes1
 plot_iso(psi, X, true, true)
-
-## Classification 
 
 # Params 
 N = 4
@@ -40,19 +34,16 @@ markersize = 400
 # This finds all vortex points intersecting the planes in 3 directions
 @time vorts_3d = findvortices3D_itp(psi, X, N) 
 plot_iso(psi, X, true, true)
-scatterVortsOnIso(vorts_3d, markersize)
+scatterVortsOnIso(vorts_3d, 10)
 
 
 # This creates an array of sets of connected vortices unordered
-@time vorts_class = setMethodPeriodic(vorts_3d, X, 0., N, true) 
+@time vorts_class = setMethodPeriodic(vorts_3d, X, 0., N, true)
 plot_iso(psi, X, true, true)
 scatterClassifiedVortices(vorts_class, vorts_3d, X, markersize)
-
 
 # This orders the vortices
 @time v_sort = sort_classified_vorts4(vorts_class, vorts_3d, X); 
 plot_iso(psi, X, true, true)
 periodicPlotting(v_sort, X)
-
-
 
